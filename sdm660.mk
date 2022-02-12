eccfcfe49d87ee4e2a16fa153fa354f067f99548
Subhajeet Muhuri <subhajeet.muhuri@aosip.dev>
2022-02-13 00:26:40 +0530

    sdm660-common: Add atrace-hal build rule
    Change-Id: Icbf69cfe562c7704dfe31c610f458b0f3961be07

--- sdm660.mk
+++ sdm660.mk
@@ -81,6 +81,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
 
+# ATRACE_HAL
+PRODUCT_PACKAGES += \
+    android.hardware.atrace@1.0-service
+
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio@2.0-impl \
