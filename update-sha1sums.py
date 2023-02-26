40f48cde52edc4a48f0d95337f08cbeb209ad289
z3zens <nerd.z3zens@gmail.com>
2023-02-26 00:44:18 +0000

    sdm660-common: Initial bringup for base AOSP
    * Change's Copyright from LinegaeOS to Android Open Source Project
    * Nuked Trust HAL and LiveDisplay

--- update-sha1sums.py
+++ update-sha1sums.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 #
-# Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017-2020 The LineageOS Project
+# Copyright (C) 2023 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
